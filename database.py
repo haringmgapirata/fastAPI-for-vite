d7614840bf44b9932fd22a2e710bd7d3a9ae2619
haringmgapirata <rexbellilumantao13@gmail.com>
2025-04-16 22:50:19 +0800

    first commit

---
+++ database.py
@@ -0,0 +1,13 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+database_url = os.environ.get("DATABASE_URL")
+
+if not database_url:
+    raise ValueError("DATABASE_URL environment variable not set")
+
+engine = create_engine(database_url, echo=True)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
