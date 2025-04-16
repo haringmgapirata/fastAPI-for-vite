6b2ac394ebd196ffbc7e5bce66cf9f487e687037
haringmgapirata <rexbellilumantao13@gmail.com>
2025-04-16 22:56:18 +0800

    fixed database url

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-database_url = os.environ.get("DATABASE_URL")
+database_url = os.environ.get("postgresql://vitepostgresql_user:gKS2S5YyLABhftvQGmpeCMUBg7fPdCfW@dpg-cvvrdaa4d50c739lu7fg-a/vitepostgresql")
 
 if not database_url:
     raise ValueError("DATABASE_URL environment variable not set")
